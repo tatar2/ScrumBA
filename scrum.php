576bf5c1a99b698937954c3d2547239f54253db7
Tatar <tatarzb@poczta.onet.pl>
2014-05-28 00:49:46 +0200

    More on database design

--- scrum.php
+++ scrum.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 //Run controller depending of the view selected. Create controller object and call controller's action.
+//v - view
+//a - action
 switch ($_GET['v']) {
   case 'index': 
     require 'controllers/controller_index.php';
